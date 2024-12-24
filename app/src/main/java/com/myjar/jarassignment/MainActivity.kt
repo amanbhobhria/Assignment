36867a3c458030f8d078e52698cff278effbdbc8
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2024-12-24 12:10:41 +0530

    SearchView implemnted and version change of  agp to 8.4.2
    SearchView implemnted and version change of  agp to 8.4.2

--- app/src/main/java/com/myjar/jarassignment/MainActivity.kt
+++ app/src/main/java/com/myjar/jarassignment/MainActivity.kt
@@ -2,6 +2,7 @@ package com.myjar.jarassignment
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel by viewModels<JarViewModel>()
     private lateinit var adapter: ListAdapter<ComputerItem, *>
+    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +32,25 @@ class MainActivity : ComponentActivity() {
 
         setupUi()
         observeFlows()
+        setUpSearchView()
     }
 
+
+    private fun setUpSearchView(){
+        searchView = findViewById(R.id.searchView)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                val filteredList = adapter.currentList.filter {
+                    it.name.contains(p0.toString(), ignoreCase = true)
+                }
+                adapter.submitList(filteredList)
+                return false
+            }
+        } )}
     private fun observeFlows() {
         lifecycleScope.launch {
             viewModel.listStringData.collectLatest {
